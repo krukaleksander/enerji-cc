ede5cb89a6d1c1804945840dc24df5d6c2ef1fb6
Alek Kruk <kruk.aleksander@hotmail.com>
2019-11-16 15:10:54 +0100

    basic app ready to push on hiroku

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,10 @@ router.get('/', function (req, res, next) {
     title: 'EnerjiCC Panel'
   });
 });
+// musisz podać dokładną ścieżkę ulokowania tego!
+router.get('javascripts/calculator.js', function (req, res) {
+  res.sendfile(__dirname + '/../public/javascripts/calculator.js');
+});
 router.post('/', function (req, res) {
   const body = req.body;
   if (body.login === login && body.password === password) {
--- routes/panel.js
+++ routes/panel.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+
 router.all('*', (req, res, next) => {
     if (!req.session.admin) {
         res.redirect('/');
@@ -13,10 +14,15 @@ router.get('/', function (req, res, next) {
 
     // req.session.admin = 0; - zlikwidowanie sesji
     res.render('panel', {
-        title: 'EnerjiCC Panel'
-    });
+        title: 'EnerjiCC Panel',
 
+    });
 });
+router.get('/panel/calculator.js', function (req, res) {
+    // res.sendfile(__dirname + '../public/javascripts/calculator.js');
+    console.log('poporisłem grzecznie o skrypt i musisz mi go wydać')
+});
+
 router.post('/', function (req, res, next) {
     req.session.admin = 0;
     res.redirect('/');
