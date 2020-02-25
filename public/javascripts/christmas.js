77a7aa21f5d92e1a5b7fac2faafe4eed8317f7bb
Alek Kruk <kruk.aleksander@hotmail.com>
2020-02-25 06:08:43 +0100

    time to easter counting

--- public/javascripts/christmas.js
+++ public/javascripts/christmas.js
@@ -1,10 +1,11 @@
-// const christmas = new Date(2020, 0, 1);
-// const today = new Date();
-// const timeToChristmas = christmas - today;
-// const daysToChristmas = Math.floor(timeToChristmas / 86400000);
-// const hoursToChristmas = Math.floor((timeToChristmas - (daysToChristmas * 86400000)) / 3600000);
-// const minutesToChristmas = Math.floor((timeToChristmas - (daysToChristmas * 86400000) - (hoursToChristmas * 3600000)) / 60000);
+const christmas = new Date(2020, 3, 12);
+const today = new Date();
+const timeToChristmas = christmas - today;
+const daysToChristmas = Math.floor(timeToChristmas / 86400000);
+const hoursToChristmas = Math.floor((timeToChristmas - (daysToChristmas * 86400000)) / 3600000);
+const minutesToChristmas = Math.floor((timeToChristmas - (daysToChristmas * 86400000) - (hoursToChristmas * 3600000)) / 60000);
 
-// document.querySelector('.days-christmas').innerText = ` ${daysToChristmas} dni`;
-// document.querySelector('.hours-christmas').innerText = ` ${hoursToChristmas}H`;
-// document.querySelector('.minutes-christmas').innerHTML = ` ${minutesToChristmas}min <i class="fas fa-glass-cheers"></i>`;
+document.querySelector('.days-christmas').innerText = ` ${daysToChristmas} dni`;
+document.querySelector('.hours-christmas').innerText = ` ${hoursToChristmas}H`;
+document.querySelector('.minutes-christmas').innerHTML = ` ${minutesToChristmas}min <i class="fas fa-egg"></i>`;
+// <i class="fas fa-glass-cheers"></i>
