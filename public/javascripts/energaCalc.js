75c672dffbbae7a3d14b1cd568fd0490513f6721
Alek Kruk <kruk.aleksander@hotmail.com>
2020-03-21 10:21:53 +0100

    basic engine of calc ready

--- public/javascripts/energaCalc.js
+++ public/javascripts/energaCalc.js
@@ -5,44 +5,31 @@ const alertPriceNow = document.getElementById('alertPriceNow');
 const alertPriceInTariff = document.getElementById('alertPriceInTariff');
 const alertWear = document.getElementById('alertWear');
 const alertProposePrice = document.getElementById('alertProposePrice');
+const btnCopyCalc = document.getElementById('btnCopyCalc');
 
 let wearFactor = 1;
 let tariff, endOfAgreement, priceNow, priceInTariff, wear, proposePrice, margeMass;
 
 replaceCommasAndParseFn = () => {
-    priceInTariff = parseFloat(priceInTariff.replace(/\,/g, '.'));
-    wear = parseFloat(wear.replace(/\,/g, '.'));
-    priceNow = parseFloat(priceNow.replace(/\,/g, '.'));
-    proposePrice = parseFloat(proposePrice.replace(/\,/g, '.'));
+    priceInTariff.value = parseFloat(priceInTariff.value.replace(/\,/g, '.'));
+    wear.value = parseFloat(wear.value.replace(/\,/g, '.'));
+    priceNow.value = parseFloat(priceNow.value.replace(/\,/g, '.'));
+    proposePrice.value = parseFloat(proposePrice.value.replace(/\,/g, '.'));
 }
 verifyValuesFn = () => {
-    if (typeof endOfAgreement != "number") {
-        alertEndOfAgreement.innerText = "podaj datę w cyfrach..."
-    } else if (endOfAgreement > 2022 || endOfAgreement < 2019) {
+    if (parseInt(endOfAgreement.value) > 2022 || parseInt(endOfAgreement.value) < 2019) {
         alertEndOfAgreement.innerText = "napewno taka data jest w pliku?"
-    }
-    if (typeof priceNow != "number") {
-        alertPriceNow.innerText = "cyfrę jeśli łaska.."
-    }
-    if (typeof priceInTariff != "number") {
-        alertPriceInTariff.innerText = "cyfrę jeśli łaska.."
-    }
-    if (typeof wear != "number") {
-        alertWear.innerText = "cyfrę jeśli łaska.."
-    }
-    if (typeof proposePrice != "number") {
-        alertProposePrice.innerText = "cyfrę jeśli łaska.."
     }
 }
 
 getActualValuesFn = () => {
-    tariff = document.getElementById('tariff').value;
-    endOfAgreement = parseInt(document.getElementById('endOfAgreement').value);
-    priceNow = document.getElementById('priceNow').value;
-    priceInTariff = document.getElementById('priceInTariff').value;
-    wear = document.getElementById('wear').value;
-    proposePrice = document.getElementById('proposePrice').value;
-    switch (endOfAgreement) {
+    tariff = document.getElementById('tariff');
+    endOfAgreement = document.getElementById('endOfAgreement');
+    priceNow = document.getElementById('priceNow');
+    priceInTariff = document.getElementById('priceInTariff');
+    wear = document.getElementById('wear');
+    proposePrice = document.getElementById('proposePrice');
+    switch (parseInt(endOfAgreement.value)) {
         case 2021:
             wearFactor = 1;
             break;
@@ -60,7 +47,7 @@ getActualValuesFn = () => {
 }
 
 calcMargeMass = () => {
-    margeMass = Math.floor((proposePrice - priceInTariff) * wear * wearFactor);
+    margeMass = Math.floor((proposePrice.value - priceInTariff.value) * wear.value * wearFactor);
 }
 
 calcFn = () => {
@@ -68,7 +55,7 @@ calcFn = () => {
     replaceCommasAndParseFn();
     verifyValuesFn();
     calcMargeMass();
-    summaryCalc.innerText = `Grupa taryfowa: ${tariff}, Umowa kończy się: ${endOfAgreement}, Klient posiada aktualnie cenę ${priceNow}, Cena w cenniku dla taryfy ${tariff}: ${priceInTariff}, Zużycie roczne: ${wear} MWh. Propozycja cenowa: ${proposePrice}, Masa marży: ~ ${margeMass} `
+    summaryCalc.innerHTML = `Grupa taryfowa: <span class ="value-of-calc-data">${tariff.value}</span>, Umowa kończy się: <span class ="value-of-calc-data">${endOfAgreement.value}</span>, Klient posiada aktualnie cenę <span class="value-of-calc-data">${priceNow.value}</span>, Cena w cenniku dla taryfy <span class ="value-of-calc-data">${tariff.value}</span>: <span class ="value-of-calc-data">${priceInTariff.value}</span>, Zużycie roczne: <span class ="value-of-calc-data">${wear.value}</span> MWh. Propozycja cenowa: <span class ="value-of-calc-data">${proposePrice.value}</span>, Mnożnik marży <span class ="value-of-calc-data">${wearFactor}</span>, Masa marży: ~ <span class ="value-of-calc-data marge-mass-span">${margeMass}</span> `
 }
 
 
