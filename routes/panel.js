fbc6180c0100d5e456bfc59d1c882fa891b51fec
Alek Kruk <kruk.aleksander@hotmail.com>
2020-04-13 08:40:24 +0200

    backend for all new tariifs ready

--- routes/panel.js
+++ routes/panel.js
@@ -30,7 +30,6 @@ router.post('/', function (req, res, next) {
 
 router.post('/change-price/', (req, res, next) => {
     const requestBody = req.body;
-    console.log(requestBody);
     const removeCommasFn = (expression) => {
         return expression.replace(/\,/g, '.');
     }
@@ -77,7 +76,91 @@ router.post('/change-price/', (req, res, next) => {
 
                 }
             }, function () {
-                res.redirect('/panel')
+
+                Mamjuzdosc.findOneAndUpdate({
+                    id: "3"
+                }, {
+                    "$set": {
+                        "tariff.price2022": removeCommasFn(requestBody.c21price2022),
+                        "tariff.price2021": removeCommasFn(requestBody.c21price2021),
+                        "tariff.price2020": removeCommasFn(requestBody.c21price2020)
+
+                    }
+                }, function () {
+
+                    Mamjuzdosc.findOneAndUpdate({
+                        id: "4"
+                    }, {
+                        "$set": {
+                            "tariff.price2022.avr": removeCommasFn(requestBody.pricec22a2022Avr),
+                            "tariff.price2022.first": removeCommasFn(requestBody.pricec22a2022First),
+                            "tariff.price2022.second": removeCommasFn(requestBody.pricec22a2022Second),
+                            "tariff.price2021.avr": removeCommasFn(requestBody.pricec22a2021Avr),
+                            "tariff.price2021.first": removeCommasFn(requestBody.pricec22a2021First),
+                            "tariff.price2021.second": removeCommasFn(requestBody.pricec22a2021Second),
+                            "tariff.price2020.avr": removeCommasFn(requestBody.pricec22a2020Avr),
+                            "tariff.price2020.first": removeCommasFn(requestBody.pricec22a2020First),
+                            "tariff.price2020.second": removeCommasFn(requestBody.pricec22a2020Second),
+
+                        }
+                    }, function () {
+
+                        Mamjuzdosc.findOneAndUpdate({
+                            id: "5"
+                        }, {
+                            "$set": {
+                                "tariff.price2022.avr": removeCommasFn(requestBody.pricec22b2022Avr),
+                                "tariff.price2022.first": removeCommasFn(requestBody.pricec22b2022First),
+                                "tariff.price2022.second": removeCommasFn(requestBody.pricec22b2022Second),
+                                "tariff.price2021.avr": removeCommasFn(requestBody.pricec22b2021Avr),
+                                "tariff.price2021.first": removeCommasFn(requestBody.pricec22b2021First),
+                                "tariff.price2021.second": removeCommasFn(requestBody.pricec22b2021Second),
+                                "tariff.price2020.avr": removeCommasFn(requestBody.pricec22b2020Avr),
+                                "tariff.price2020.first": removeCommasFn(requestBody.pricec22b2020First),
+                                "tariff.price2020.second": removeCommasFn(requestBody.pricec22b2020Second),
+
+                            }
+                        }, function () {
+
+                            Mamjuzdosc.findOneAndUpdate({
+                                id: "6"
+                            }, {
+                                "$set": {
+                                    "tariff.price2022": removeCommasFn(requestBody.b21price2022),
+                                    "tariff.price2021": removeCommasFn(requestBody.b21price2021),
+                                    "tariff.price2020": removeCommasFn(requestBody.b21price2020)
+
+                                }
+                            }, function () {
+
+                                Mamjuzdosc.findOneAndUpdate({
+                                    id: "7"
+                                }, {
+                                    "$set": {
+                                        "tariff.price2022.avr": removeCommasFn(requestBody.priceb222022Avr),
+                                        "tariff.price2022.first": removeCommasFn(requestBody.priceb222022First),
+                                        "tariff.price2022.second": removeCommasFn(requestBody.priceb222022Second),
+                                        "tariff.price2021.avr": removeCommasFn(requestBody.priceb222021Avr),
+                                        "tariff.price2021.first": removeCommasFn(requestBody.priceb222021First),
+                                        "tariff.price2021.second": removeCommasFn(requestBody.priceb222021Second),
+                                        "tariff.price2020.avr": removeCommasFn(requestBody.priceb222020Avr),
+                                        "tariff.price2020.first": removeCommasFn(requestBody.priceb222020First),
+                                        "tariff.price2020.second": removeCommasFn(requestBody.priceb222020Second),
+
+                                    }
+                                }, function () {
+
+                                    res.redirect('/panel')
+
+                                })
+
+                            })
+
+                        })
+
+                    })
+
+                })
 
             })
 
