d642e28587dc80b048d7f6fc120144e30569ba34
Alek Kruk <kruk.aleksander@hotmail.com>
2020-09-27 19:27:19 +0200

    add autoumn bg and ripple btn

---
+++ public/javascripts/smallparts/rippleBtn.js
@@ -0,0 +1,23 @@
+(() => {
+    console.log('script activated');
+    const rippleBtns = [...document.querySelectorAll('.ripple-container__btn')];
+
+    rippleBtns.forEach(btn => {
+        btn.addEventListener('click', function (e) {
+
+            let x = e.clientX - e.target.offsetLeft;
+            let y = e.clientY - e.target.offsetTop;
+
+            let ripples = document.createElement('span');
+            ripples.classList.add('ripple-container__ripple');
+            ripples.style.left = x + 'px';
+            ripples.style.top = y + 'px';
+            console.log(this);
+            this.appendChild(ripples);
+
+            setTimeout(() => {
+                ripples.remove();
+            }, 1000)
+        });
+    });
+})();
--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ router.get('/', function (req, res, next) {
     login = data[0].login;
     password = data[0].password;
     res.render('index', {
-      title: '🔥 EnerjiCC Panel 🔥',
+      title: '🔥 P5panel 🔥',
     });
 
   });
