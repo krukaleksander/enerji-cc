906eaaf9b4b2c8afe8fc29ae5ff72b62b56f0d9e
Alek Kruk <kruk.aleksander@hotmail.com>
2021-01-30 11:47:36 +0100

    calc margeMass ok

--- public/javascripts/smallparts/gazomierz.js
+++ public/javascripts/smallparts/gazomierz.js
@@ -1,9 +1,8 @@
 (() => {
-
     const insertPriceBtn = document.querySelector('.gazomierz__insert-price');
     const insertPriceContainer = document.querySelector('.insert-price');
     const closeInsertPriceContainer = document.getElementById('closeInsertGazPrice');
-    const calcBtn = document.querySelectorAll('.gazomierz__calc');
+    const calcBtn = document.querySelector('.gazomierz__calc');
 
     const replaceAndParseMainFn = (item) => {
         return parseFloat(item.replace(/\,/g, '.'));
@@ -18,9 +17,21 @@
         const gazActualPrice = replaceAndParseMainFn(document.getElementById('gazActualPrice').value);
         const gazMwh = replaceAndParseMainFn(document.getElementById('gazMwh').value);
         const gazProposition = replaceAndParseMainFn(document.getElementById('gazProposition').value);
+        const priceDbGaz = replaceAndParseMainFn(document.getElementById('priceDbGaz').value);
+        const agreementStart = replaceAndParseMainFn(document.getElementById('agreementStart').value);
+        const endOfNewAgreementGaz = replaceAndParseMainFn(document.getElementById('endOfNewAgreementGaz').value);
+        const partOfFirstYear = replaceAndParseMainFn(document.getElementById('agreementStartGazMonth').value);
+
 
         const savingsFromOh = (gazOh * 12).toFixed(2);
         const savingsFromMwh = ((gazActualPrice - gazProposition) * gazMwh).toFixed(2);
+
+        const differenceInPrice = (gazProposition - priceDbGaz).toFixed(2);
+
+        const factor = ((endOfNewAgreementGaz - agreementStart) + 1 - (partOfFirstYear / 12).toFixed(2));
+
+
+        const margeMass = (differenceInPrice * gazMwh * factor).toFixed(2);
 
 
     };
