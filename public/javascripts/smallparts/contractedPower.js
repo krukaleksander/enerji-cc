4ea699c8636becbb432b713950e81dccdde7c28d
Alek Kruk <kruk.aleksander@hotmail.com>
2021-02-28 07:51:46 +0100

    max ammout mwhs engine, showing, hiding fine

---
+++ public/javascripts/smallparts/contractedPower.js
@@ -0,0 +1,30 @@
+(() => {
+
+    const container = document.querySelector('.contracted-power');
+    const showBtn = document.getElementById('showCalcMwhs');
+    const closeBtn = document.querySelector('.contracted-power__close');
+
+    showBtn.addEventListener('click', () => {
+        container.style.display = 'flex';
+    });
+
+    closeBtn.addEventListener('click', () => {
+        container.style.display = 'none';
+    })
+
+    // część odpowiadająca za obliczenia
+    const calcBtn = document.getElementById('btnCalcMwhs');
+    calcBtn.addEventListener('click', () => {
+        const b5 = 140;
+        const c5 = 400;
+        const d5 = 12;
+        const e5 = 12;
+        const f5 = 0.4;
+        const mwhs = Number(document.querySelector('.contracted-power__input').value);
+        console.log(mwhs)
+        const scoreParagraph = document.querySelector('.contracted-power__score');
+        const score = (mwhs / b5) * c5 * (d5 / e5) * f5;
+        scoreParagraph.innerHTML = score;
+    });
+
+})()
