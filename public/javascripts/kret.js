dddf3471d072de03bdd179e0113c2a8f76594cd0
Alek Kruk <kruk.aleksander@hotmail.com>
2021-04-06 17:07:35 +0200

    kret security update

--- public/javascripts/kret.js
+++ public/javascripts/kret.js
@@ -1,14 +1,21 @@
 (() => {
     //pokazywanie / chowanie kreta
     const kret = document.querySelector('.kret');
-
+    let shiftFlag = false;
+    let altFlag = false;
     window.addEventListener('keydown', (e) => {
-        if (e.keyCode === 107) {
-            kret.style.display = 'flex';
+        if (e.keyCode === 17) {
+            shiftFlag = true;
+        }
+        if (e.keyCode === 16) {
+            altFlag = true;
         }
         if (e.keyCode === 27) {
             kret.style.display = 'none';
+            shiftFlag = false;
+            altFlag = false;
         }
+        if (shiftFlag && altFlag) kret.style.display = 'flex';
     })
     // koniec pokazywanie / chowanie kreta
 
