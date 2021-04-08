342948432fe67c102435d1b3c4c82c31dabb5374
Alek Kruk <kruk.aleksander@hotmail.com>
2021-04-08 19:21:44 +0200

    getting from db, spinner, ok. problem with client ammount

--- public/javascripts/crmGetClients.js
+++ public/javascripts/crmGetClients.js
@@ -2,6 +2,7 @@
     const table = document.querySelector('.clients-table');
     const spinner = document.querySelector('.lds-roller');
     const clientsTable = document.querySelector('.clients-table tbody');
+    const numberOfClientsSpan = document.querySelector('.wallet-summary span');
     const getClients = async (start, end) => {
         const clients = await fetch(`${window.location.href}/get-clients/`);
         const clientsArr = await clients.json();
@@ -18,6 +19,7 @@
         });
         spinner.style.display = 'none';
         table.style.display = 'block';
+        numberOfClientsSpan.innerHTML = `${clientsArr.length} klientów. Gratuluję wspólniku =)`;
     }
     getClients(0, 20);
 
