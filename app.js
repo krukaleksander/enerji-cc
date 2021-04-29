3d57a92f30bae74e2bf65d935d7f4b0aec482006
Alek Kruk <kruk.aleksander@hotmail.com>
2021-04-29 07:49:00 +0200

    add icon

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
+app.use('/favicon.ico', express.static('/favicon.ico'));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({
